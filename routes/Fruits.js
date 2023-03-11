7365ef65b133ec5f41c3d7e4ddc7065a293f471c
Ciaraerica23 <ciaraestafford@gmail.com>
2023-03-11 09:47:19 -0500

    added crud methods

--- routes/Fruits.js
+++ routes/Fruits.js
@@ -19,11 +19,28 @@ let fruits = [
         color: "Purple"
     },
 ]
+router.use(express.json());
+router.use(express.urlencoded({extended:true}));   
 router.get('/',(req,res)=>{
     res.json(fruits)
 })
 router.get('/:id',(req,res)=>{
     const id = req.params.id-1
     res.json(fruits[id])
+})
+router.post('/',(req,res)=>{
+    fruits.push(req.body)
+    res.json(fruits)
+})
+router.put('/:id',(req,res)=>{
+    const id = req.params.id
+    fruits.splice(id-1,1)
+    fruits.splice(id-1,0,req.body)
+    res.json(fruits)
+})
+router.delete('/:id',(req,res)=>{
+    const id = req.params.id
+    fruits.splice(id-1,1)
+    res.json(fruits)
 })
 module.exports = router
--- routes/Users.js
+++ routes/Users.js
@@ -19,12 +19,28 @@ let users = [
         age: 22
     }
 ]
-
+router.use(express.json());
+router.use(express.urlencoded({extended:true}));   
 router.get('/',(req,res)=>{
     res.json(users)
 })
 router.get('/:id',(req,res)=>{
     const id = req.params.id-1
     res.json(users[id])
+})
+router.post('/',(req,res)=>{
+    users.push(req.body)
+    res.json(users)
+})
+router.put('/:id',(req,res)=>{
+    const id = req.params.id
+    users.splice(id-1,1)
+    users.splice(id-1,0,req.body)
+    res.json(users)
+})
+router.delete('/:id',(req,res)=>{
+    const id = req.params.id
+    users.splice(id-1,1)
+    res.json(users)
 })
 module.exports = router
